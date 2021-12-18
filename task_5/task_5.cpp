6a78ddfe1d2cd607f96f5db2f50c8d3d68a91662
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2021-12-18 19:06:44 +0200

    Add project files.

---
+++ task_5/task_5.cpp
@@ -0,0 +1,16 @@
+﻿/*
+Задание 5. Напишите программу, которая вычисляем
+объём шара.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, ""); //работает только с потоком вывода	
+
+	return	0;
+}
